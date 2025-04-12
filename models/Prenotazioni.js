98cb4d8f815ced0116178339be1cf90514de5778
adrianoastarita <66206109+adrianoastarita@users.noreply.github.com>
2025-04-12 19:14:27 +0000

    upd ok

--- models/Prenotazioni.js
+++ models/Prenotazioni.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const prenotazioni_schema = new mongoose.Schema({
     bookingID: { type: mongoose.Schema.Types.ObjectId, ref: 'Booking', required: true },
     partnerID: { type: mongoose.Schema.Types.ObjectId, ref: 'Partner', required: true },
-    data: { type: Date, required: true },
+    DataView: { type: Date, required: true },
     coperti: {type: Number, required: true}
 });
 
-module.exports = mongoose.model('Partner', partner_schema, 'partner');
+module.exports = mongoose.model('Prenotazioni', prenotazioni_schema, 'prenotazioni');
